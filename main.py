a7112596b6c54ed0345963f30e57040a4a7c528f
joshuaaduarte <jad6442@tamu.edu>
2022-09-08 13:38:07 -0700

    Updated script to ensure user input matches what is required. Began accounting for edge cases regarding the creation of the initial csv. Created clear function to enhance user experience by removing unnecessary values presented to them in the output.

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 # imports 
 import csv #used to manipulate csv files
+import os
 import os.path # used to check if files exist 
 # gather initial user information
 
+# Basic Functions
+def clear():
+    os.system('cls')
+
 # Conversions
 lbToKg = 0.45359237
+kgTolb = 1/lbToKg
 inToCm = 2.54
+cmToIn = 1/inToCm
 kgToCal = 7700
 
 # ask user to input their first and last name
 firstName = input('Enter first name: ')
 lastName = input('Enter last name: ')
 
+clear()
+
 # check if previous csv file exists
 if os.path.exists('{}_{}.csv'.format(firstName.lower(), lastName.lower())) == True:
     done = True
     goals = True
+    print('Welcome Back! Your file already exists.')
 else:
     done = False
     goals = False
@@ -24,23 +34,40 @@ else:
 while not done:
 
     age = int(input('Enter Age: '))
-    gender = input('Enter gender (M/F): ')
-    units = input("Do you use kg or lbs? ")
-    if units.lower() == 'kg':
-        weight = float(input('Enter Weight: '))
-        height = float(input('Enter Height in cm: '))
-    else:
-        weight = float(input('Enter Weight: ')) * lbToKg
-        heightFeet = (input('Enter Height (ft-in): ')).split('-')
-        height = ((int(heightFeet[0]) * 12) + int(heightFeet[1])) * inToCm
 
-    if gender.lower() == 'm':
-        bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5
-    else:
-        bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161
+    # ensures input is either m or f
+    genderTextOk = False
+    while genderTextOk == False:
+        gender = input('Enter gender (M/F): ')
+        if gender.lower() == 'm':
+            bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5
+            genderTextOk = True
+        elif gender.lower() == 'f':
+            bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161
+        else: 
+            print('Input Invalid. Please try again')
+
+    # ensures input is either 1 or 2
+    unitTextOk = False
+    while unitTextOk == False:
+        units = input("Do you use kg or lbs?(1/2) ")
+        if units == '1':
+            unitTextOk == True
+            weight = float(input('Enter Weight: '))
+            height = float(input('Enter Height in cm: '))
+        elif units == '2':
+            unitTextOk == True
+            weight = float(input('Enter Weight: ')) * lbToKg
+            heightFeet = (input('Enter Height (ft-in): ')).split('-')
+            height = ((int(heightFeet[0]) * 12) + int(heightFeet[1])) * inToCm
+        else:
+            print('Input Invalid. Please try again')
+
+    
+    
 
     print('first name:', firstName, 'last name:', lastName, 'age:', age, 'height:', round(height, 2), 'weight:',
-          round(weight, 2), 'BMR:', round(bmr, 2))
+          round(weight, 2))
 
     correct = input('Does this look right? (Y/N): ')
     if correct.lower() == 'y':
@@ -52,7 +79,7 @@ while not done:
 while not goals:
     goalLoss = input("Do you wish to maintain weight, lose fat, lose extreme amount of fat (M/L/E): ")
     if goalLoss.lower() == 'm':
-        print("Keep on doin' what your doin':)")
+        print("Eat ", bmr, ' calories per day!')
     else:
         if units.lower() == 'kg':
             goalWeight = float(input('Enter goal weight: '))
@@ -81,7 +108,7 @@ while not goals:
 if os.path.exists('{}_{}.csv'.format(firstName.lower(), lastName.lower())) == False:
     header = ['First Name', ' Last Name', 'Gender','Age', 'Height', 'Weight', 'BMR', 'Goal Weight','Daily Caloric Intake', 'Preferred Units']
     data = [firstName, lastName, gender, age, height, weight, bmr, goalWeight, dailyCalorieIntake, units]
-    with open('{}_{}.csv'.format(firstName, lastName), 'w', newline='') as file:
+    with open('{}_{}.csv'.format(firstName.lower(), lastName.lower()), 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(header)
         writer.writerow(data)
